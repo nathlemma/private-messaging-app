0b71b436520945545081b2387ef7e827bc4faae4
Nathan <42325916+nathlemma@users.noreply.github.com>
2023-06-20 20:18:05 -0400

    hover feature

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,19 @@
 import "../styles.scss";
-const Chats = ()=>{
-    return(
-        <div className="chats">Chats</div>
-    )
-}
-export default Chats
+import cam from "../images/cam.png";
+import more from "../images/more.png";
+import add from "../images/add.png";
+
+const Chats = () => {
+  return (
+    <div className="chats">
+      <div className="c-nav">
+        <div className="c-icons">
+          <img src={cam} />
+          <img src={add} />
+          <img src={more} />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Chats;
