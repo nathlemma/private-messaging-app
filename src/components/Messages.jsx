2e4f5e7a2ff51d7cc5356e8df41f4c2a10a8f6a2
Nathan <42325916+nathlemma@users.noreply.github.com>
2023-06-24 16:51:43 -0400

    message toggle between user and friend successful

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,23 +1,44 @@
 import default_pfp from "../images/default-pfp.jpg";
+import { useState } from "react";
 
 const Message = () => {
-   
+  const [isActive, setIsActive] = useState(false);
+  const handleClick = () => {
+    setIsActive(!isActive);
+    console.log("Div clicked!");
+  };
+  const myclass = () => {
+    let baseClass = "message-container";
+    return isActive ? `${baseClass} friend` : `${baseClass} user`;
+  };
   return (
-    <div className="message-cont">
-      <img
-        src={default_pfp}
-        className="m-pfp"
-        style={{ height: "20px", width: "20px" }}
-      />
-      <p>hey how are you</p>
+    <div onClick={handleClick} className={myclass()}>
+      <div className="message-header">
+        <img src={default_pfp} className="m-pfp" />
+        <span>1 min ago</span>
+      </div>
+      <div className="message-content">
+        <p>
+          hey how are you hey how are you hey how are you hey how are you hey
+          how are you hey how are you hey how are you hey how are you hey how
+          are you hey how are you hey how are you hey how are you hey how are
+          you hey how are you hey how are you hey how are you hey how are you
+          hey how are you hey how are youhey how are you hey how are you hey how
+          are you hey how are you
+        </p>
+        <img
+          src="https://asia.olympus-imaging.com/content/000107507.jpg"
+          className="m-pfp"
+        />
+      </div>
     </div>
   );
 };
 const Messages = () => {
   return (
-    <div className="messages-cont">
+    <div className="messages-container">
       <Message />
-      <Message  />
+      <Message />
       <Message />
       <Message />
     </div>
